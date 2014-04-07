5fd90d06af16a4da96652802d3daa0311fea19ce
Apple <apple@Family-Room-Table-Right-4-LB-MacBook-Pro.local>
2014-04-07 18:52:51 -0400

    pep8

--- euler_util.py
+++ euler_util.py
@@ -8,15 +8,15 @@ def sieve(size):
         l[k] = 0
         k += 2
     k = 3
-    while(k < size):
+    while k < size:
         m = 2
-        while(k*m < size):
+        while k*m < size:
             l[k*m] = 0
             m += 1
         k += 2
     l2 = list()
     for i in xrange(0,size):
-        if(l[i] == 1):
+        if l[i] == 1:
             l2.append(i)
     return l2
 
@@ -27,12 +27,12 @@ def gcd(a,b):
 
 def factor(n):
     l = list()
-    while(n%2==0):
+    while n%2==0:
         n = n/2
         l.append(2)
     i = 3
-    while(n != 1):
-        while(n%i==0):
+    while n != 1:
+        while n%i==0:
             n = n/i
             l.append(i)
         i += 2
@@ -42,7 +42,7 @@ def numdivisors(n):
     l = factor(n)
     d = dict()
     for i in l:
-        if(i not in d):
+        if i not in d:
             d[i] = 0
         d[i] = d[i] + 1
     prod = 1
@@ -54,8 +54,8 @@ def properdivisors(n):
     l = list()
     k = 1
     top = ceil(n**0.5)
-    while(k < top):
-        if(n%k==0):
+    while k < top:
+        if n%k==0:
             l.append(k)
             l.append(n/k)
         k += 1
